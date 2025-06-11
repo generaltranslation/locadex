b8592bea5c142b200273c73b63800fad15e349cc
Brian Lou <69982825+brian-lou@users.noreply.github.com>
2025-06-10 17:22:32 -0700

    fix(ci): Fix release script (#10)
    * Fix release script
    
    * add comment

---
+++ publish.ts
@@ -0,0 +1,43 @@
+// This file is MIT licensed and taken from https://raw.githubusercontent.com/changesets/action/04d574e831923498156e0b2b93152878063203a3/scripts/release.ts
+import { exec, getExecOutput } from '@actions/exec';
+import path from 'node:path';
+
+import pkgJson from './package.json' with { type: 'json' };
+
+const tag = `v${pkgJson.version}`;
+const releaseLine = `v${pkgJson.version.split('.')[0]}`;
+
+process.chdir(path.join(import.meta.dirname, '..'));
+
+(async () => {
+  const { exitCode, stderr } = await getExecOutput(
+    `git`,
+    ['ls-remote', '--exit-code', 'origin', '--tags', `refs/tags/${tag}`],
+    {
+      ignoreReturnCode: true,
+    }
+  );
+  if (exitCode === 0) {
+    console.log(
+      `Action is not being published because version ${tag} is already published`
+    );
+    return;
+  }
+  if (exitCode !== 2) {
+    throw new Error(`git ls-remote exited with ${exitCode}:\n${stderr}`);
+  }
+
+  await exec('git', ['checkout', '--detach']);
+  await exec('git', ['add', '--force', 'dist']);
+  await exec('git', ['commit', '-m', tag]);
+
+  await exec('changeset', ['tag']);
+
+  await exec('git', [
+    'push',
+    '--force',
+    '--follow-tags',
+    'origin',
+    `HEAD:refs/heads/${releaseLine}`,
+  ]);
+})();
